c67bc903cb8a7ca53c48fe791d447508d5243dbe
pradeepsetti <105009597+pradeepsetti@users.noreply.github.com>
2023-09-19 09:03:25 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -317,3 +317,74 @@ Step:6 1.Adding log4j2.xml file under src/test/resoures
   </Loggers>
   
 </Configuration>
+
+Step:7 Step:4(repeat) testBase Package-- BaseClass.java Class
+========================================================================================================
+package testBase;
+
+import java.time.Duration;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.apache.logging.log4j.LogManager; // ~~~~~~~~~~~~~~~~~~~~logging
+import org.apache.logging.log4j.Logger;  // ~~~~~~~~~~~~~~~~~~~~logging
+
+
+public class BaseClass {
+
+	public WebDriver driver;
+	
+	public Logger logger; // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~for logging
+
+	@BeforeClass
+	public void setup()
+	{
+		logger=LogManager.getLogger(this.getClass());  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~logging
+		
+		//ChromeOptions options=new ChromeOptions();
+		//options.setExperimentalOption("excludeSwitches",new String[] {"enable-automation"});
+		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		
+		
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//driver.get("http://localhost/opencart/upload/index.php");
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.manage().window().maximize();
+	}
+	
+	@AfterClass
+	public void tearDown()
+	{
+		//driver.quit();
+	}
+	
+	public String randomeString() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
+		return (generatedString);
+	}
+
+	public String randomeNumber() {
+		String generatedString2 = RandomStringUtils.randomNumeric(10);
+		return (generatedString2);
+	}
+	
+	public String randomAlphaNumeric() {
+		String st = RandomStringUtils.randomAlphabetic(4);
+		String num = RandomStringUtils.randomNumeric(3);
+		
+		return (st+"@"+num);
+	}
+	
+	
+}
