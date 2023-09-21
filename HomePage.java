cb055554dc6ba206fb273f6a78591a036c96841c
pradeepsetti <105009597+pradeepsetti@users.noreply.github.com>
2023-09-21 14:18:36 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1191,3 +1191,23 @@ public class DataProviders {
 	//DataProvider 4
 }
 
+Step:27 Step:22(repeat)  testng.xml for 
+========================================================================================================
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
+<suite name="OpenCartSuite">
+	
+	 <listeners>
+		<listener class-name="utilities.ExtentReportManager" />
+	</listeners>
+	
+  <test thread-count="5" name="OpenCartTest">
+	  <parameter name="browser" value="chrome" />    <!-- passing browser parameter to BaseClass ->Setup() -->
+    <classes>
+     <!--  <class name="testCases.TC_001_AccountRegistrationTest"/> -->
+     <!--   <class name="testCases.TC_002_LoginTest"/>-->
+       <class name="testCases.TC_003_LoginDataDrivenTest"/>
+       
+    </classes>
+  </test> <!-- OpenCartTest -->
+</suite> <!-- OpenCartSuite -->
