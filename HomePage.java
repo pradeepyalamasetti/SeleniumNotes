646f0ab0589c5f733d58bfff22fc1ac6310c882c
pradeepsetti <105009597+pradeepsetti@users.noreply.github.com>
2023-09-21 14:37:26 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1335,6 +1335,128 @@ public class TC_002_LoginTest extends BaseClass
 		
 	}
 	
+}
+
+
+Step:30 Step:13  Step:10(repeat)  testBase Package-- BaseClass.java Class  ---- passing config.properties
+	(groups = { "Master", "Sanity", "Regression" }) //Step8 groups added
+========================================================================================================
+
+package testBase;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;  // logging
+
+
+public class BaseClass {
+
+	public static WebDriver driver;
+	
+	public Logger logger; // for logging
+	
+	public ResourceBundle rb;// to read config.properties
+
+	@BeforeClass(groups = { "Master", "Sanity", "Regression" }) //Step8 groups added
+	@Parameters("browser")   // getting browser parameter from testng.xml
+	public void setup(String br)
+	{
+		logger=LogManager.getLogger(this.getClass());  //logging
+		
+		rb = ResourceBundle.getBundle("config");// Load config.properties
+		
+		/*ChromeOptions options=new ChromeOptions();
+		options.setExperimentalOption("excludeSwitches",new String[] {"enable-automation"});
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver(options);*/
+		
+		/*WebDriverManager.chromedriver().setup(); // Not required selenium 4.6.0 later versions
+		  driver=new ChromeDriver();*/
+		
+		//launch right browser based on parameter
+		if (br.equals("chrome")) 
+		   {
+			driver = new ChromeDriver();
+		   } 
+		else if (br.equals("edge"))
+		   {
+			driver = new EdgeDriver();
+		   } 
+		else 
+		   {
+			driver = new ChromeDriver();
+		   }
+	
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		//driver.get("https://tutorialsninja.com/demo/");
+		driver.get(rb.getString("appURL")); // get url from config.properties file
+		driver.manage().window().maximize();
+	}
+	
+	@AfterClass(groups = { "Master", "Sanity", "Regression" }) //Step8 groups added
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+	public String randomeString() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
+		return (generatedString);
+	}
+
+	public String randomeNumber() {
+		String generatedString2 = RandomStringUtils.randomNumeric(10);
+		return (generatedString2);
+	}
+	
+	public String randomAlphaNumeric() {
+		String st = RandomStringUtils.randomAlphabetic(4);
+		String num = RandomStringUtils.randomNumeric(3);
+		
+		return (st+"@"+num);
+	}
+	
+	public String captureScreen(String tname) throws IOException 
+	{
+		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+				
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png";
+
+		try 
+		{
+			FileUtils.copyFile(source, new File(destination));
+		} 
+		catch (Exception e) 
+		{
+			e.getMessage();
+		}
+		return destination;
+
+	}
+	
 	
 }
 
