cd0209a6d88b68941b6c1ac314704df6ba0bb187
pradeepsetti <105009597+pradeepsetti@users.noreply.github.com>
2023-09-21 14:30:13 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1211,3 +1211,131 @@ Step:27 Step:22(repeat)  testng.xml for
     </classes>
   </test> <!-- OpenCartTest -->
 </suite> <!-- OpenCartSuite -->
+
+
+Step:28 Step:8 Step:3(repeat) PageObjects Package -- AccountRegistrationPage.java Class --- adding loggers
+	(groups= {"Regression","Master"}) //Step8 groups added
+========================================================================================================
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+
+public class TC_001_AccountRegistrationTest extends BaseClass{
+	
+	@Test(groups= {"Regression","Master"}) //Step8 groups added
+	public void test_account_Registration() throws InterruptedException
+	{
+		logger.debug("application logs......");
+		logger.info("***  Starting TC_001_AccountRegistrationTest ***");
+		
+		try
+		{
+		HomePage hp=new HomePage(driver);
+		hp.clickMyAccount();
+		logger.info("Clicked on My account link");
+		
+		
+		hp.clickRegister();
+		logger.info("Clicked on register link");
+		
+		
+		AccountRegistrationPage regpage=new AccountRegistrationPage(driver);
+		logger.info("Providing customer data");
+		
+		regpage.setFirstName(randomeString().toUpperCase());
+		
+		regpage.setLastName(randomeString().toUpperCase());
+		
+		//regpage.setEmail(randomeString()+"@gmail.com");// randomly generated the email
+		
+		regpage.setTelephone(randomeNumber());
+		
+		String password=randomAlphaNumeric();
+		regpage.setPassword(password);
+		regpage.setConfirmPassword(password);
+		
+		regpage.setPrivacyPolicy();
+		
+		regpage.clickRadiobtn();
+		
+		regpage.clickContinue();
+		logger.info("Clicked on continue");
+		
+		String confmsg=regpage.getConfirmationMsg();
+		
+		logger.info("Validating expected message");
+		Assert.assertEquals(confmsg, "Your Account Has Been Created!","Test failed");
+		}
+		catch(Exception e)
+		{
+			logger.error("test failed");
+			Assert.fail();
+		}
+		logger.info("***  Finished TC_001_AccountRegistrationTest ***");	
+	}
+}
+
+Step:29 Step:20(repeat) testCases -- TC_002_LoginTest.java
+	(groups= {"Sanity","Master"}) //Step8 groups added
+========================================================================================================
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+
+
+public class TC_002_LoginTest extends BaseClass
+{
+	@Test(groups= {"Sanity","Master"}) //Step8 groups added
+	public void test_Login()
+	{
+		logger.info(" ***Starting TC_002_LoginTest***");
+		
+		try
+		{				
+			HomePage hp=new HomePage(driver);
+			hp.clickMyAccount();
+			logger.info("Clicked on My Account");
+			
+			hp.clickLogin();
+			logger.info("Clicked on Login Link");
+			
+			
+			LoginPage lp=new LoginPage(driver);
+			logger.info("Providing login details");
+			lp.setEmail(rb.getString("email")); // valid email, get it from config.properties file put form BaseClass.java
+			
+			lp.setPassword(rb.getString("password")); // valid password, get it from config.properties file put form BaseClass.java
+			
+			lp.clickLogin();
+			logger.info("Clicked on Login button");
+			
+			
+			MyAccountPage macc=new MyAccountPage(driver);
+			boolean targetpage=macc.isMyAccountPageExists();
+						
+			Assert.assertEquals(targetpage, true,"Invalid login data");
+			
+		}	
+		catch(Exception e)
+		{
+			Assert.fail();
+		}
+		logger.info("***Finished TC_002_LoginTest***");
+		
+	}
+	
+	
+}
+
+
