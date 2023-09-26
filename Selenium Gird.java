8c30cd4430798ceab0640ffd429771d95b5e513c
pradeepyalamasetti <105592177+pradeepyalamasetti@users.noreply.github.com>
2023-09-26 18:55:08 +0530

    Update Selenium Gird.java

--- Selenium Gird.java
+++ Selenium Gird.java
@@ -1,29 +1,6 @@
-package pageObjects;
+step:1  Download the selenium Gird version 4.13.0
+===================================================================================================
+https://www.selenium.dev/downloads/
+Save in C drive --- software folder ---- jar folder ---- add jar file
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
-public class HomePage extends BasePage {
-
-	public HomePage(WebDriver driver) {
-		super(driver);
-	}
-
-	// Elements
-	@FindBy(xpath = "//span[text()='My Account']")
-	WebElement lnkMyaccount;
-
-	@FindBy(linkText = "Register")
-	WebElement lnkRegister;
-
-	// Action Methods
-	public void clickMyAccount() {
-		lnkMyaccount.click();
-	}
-
-	public void clickRegister() {
-		lnkRegister.click();
-	}
-	
-}
