08aac85fde3b4a137a199af57d7f996c88989400
pradeepyalamasetti <105592177+pradeepyalamasetti@users.noreply.github.com>
2023-09-26 23:02:26 +0530

    Update Install softwares.java

--- Install softwares.java
+++ Install softwares.java
@@ -1,3 +1,19 @@
 1.Install Java using this link
+=====================================================================================================
 https://www.oracle.com/java/technologies/downloads/#java11-windows
-create account in Oracle.then after you can download exe file.
+  
+Download & Install Java
+-------------------------------
+step1) downlaod & install JDK
+       create account in Oracle.then after you can download exe file in 3 days
+
+step2) set jdk path 
+C:\Program Files\Java\jdk-11.0.15\bin
+
+Right click This PC --> properties-->Advanced system settings
+-->Environemnt varaibles button-->system variables
+-- select path varaible--> click on EDIT
+--> Click on New button-paste your JDK Path.
+
+step3) open command prompt
+	java -version
