6296d30c01cd63754eb0222529abce4d95d00f9d
pradeepyalamasetti <105592177+pradeepyalamasetti@users.noreply.github.com>
2023-09-26 19:00:20 +0530

    Update Selenium Gird.java

--- Selenium Gird.java
+++ Selenium Gird.java
@@ -3,4 +3,6 @@ step:1  Download the selenium Gird version 4.13.0
 https://www.selenium.dev/downloads/
 Save in C drive --- software folder ---- jar folder ---- add jar file
 
-
+step:2  Open the cmdpormt enter into the directory(particular of location)
+===================================================================================================
+cd C:\software\jars
