f9c6b13d42401ee51b79266d1633c0f9ac5eb809
pradeepyalamasetti <105592177+pradeepyalamasetti@users.noreply.github.com>
2023-10-04 14:10:02 +0530

    Update MaunalTesting_Notes.java

--- MaunalTesting_Notes.java
+++ MaunalTesting_Notes.java
@@ -1,24 +1,48 @@
 1. What is Manual Testing?
 Ans. Manual Testing is a kind of software testing in which a software tester develops and executes the test cases without using any automated testing tools.
+     
 2. What tool is used for Manual Testing?
 Ans. Manual testing tools such as Bugzilla, JIRA, JMeter, and many more.
+     
 3. What is the Advantage of Manual Testing?
 Ans. 1.Manual testing can be done on all kinds of applications.
      2.Application must be tested manually before it is automated.
-     3.It allows tester to perform adhoc testing
-     4.It is preferred in the projects where the requirements change frequently and for the products where the GUI changes constantly
+     3.It allows tester to perform adhoc testing.
+     4.It is preferred in the projects where the requirements change frequently and for the products where the GUI changes constantly.
+     
 4. What are the disadvantages of Manual testing?
 Ans. 1.time-consuming
      2.More number of human resource are required.
      3.Repeating the same task again and again is almost impossiable.
+     
 5. Will manual testing end in the future?
 Ans. 
+     
 6. What is Regression Testing?
+Ans. Regression testing is the process of retesting the software apllication to ensure that new code changes have not affected existing functioanlities.
+     
 7. How do you decide on test cases for the regression suite?
+Ans. 
+     
 8. How do you find the regression scenarios if a defect is fixed?
+Ans. By Requirement Traceability Matrix is used for the impact analysis. RTM tells you the relationship between the features.
+     Once you had done retesting after a bug fixes, you need to identify the other affected areas then execute their test cases. 
+     Similarly, once there is a new feature added check the affected area and execute the already written test cases for the affected areas.
+     
 9. You have 20 bugs, and you need to fix them as well as you need to run the regression. You are the only
-two people in a team. How will you do it, and which one will you prioritise?
-10. Difference between Regression and retesting
+   two people in a team. How will you do it, and which one will you prioritise?
+     
+10. Difference between Regression and retesting?
+Ans. Regression:
+     1.It is know as generic testing.
+     2.Regression testing is the process of retesting the software apllication to ensure that new code changes have not affected existing functioanlities.
+     3.It takes more time in performing regression testing as it explore the whole application.
+     Re-testing:
+     1.It is know as planned testing.
+     2.Testing again and again to make sure the defect has been fixed or not.
+     3.It take less time as it focus only on defect of the application.
+     
 11. If you have a situation like need to execute all test cases under the project for regression before
 release, how you can manage the situation?
-
+Ans.
+     
