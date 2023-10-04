b857f588861636ef07928007caddbf431513c3d4
pradeepyalamasetti <105592177+pradeepyalamasetti@users.noreply.github.com>
2023-10-04 12:32:53 +0530

    Update MaunalTesting_Notes.java

--- MaunalTesting_Notes.java
+++ MaunalTesting_Notes.java
@@ -1,1 +1,14 @@
+1. What is Manual Testing?
+2. What tool is used for Manual Testing?
+3. What is the Advantage of Manual Testing?
+4. What are the disadvantages of Manual testing?
+5. Will manual testing end in the future?
+6. What is Regression Testing?
+7. How do you decide on test cases for the regression suite?
+8. How do you find the regression scenarios if a defect is fixed?
+9. You have 20 bugs, and you need to fix them as well as you need to run the regression. You are the only
+two people in a team. How will you do it, and which one will you prioritise?
+10. Difference between Regression and retesting
+11. If you have a situation like need to execute all test cases under the project for regression before
+release, how you can manage the situation?
 
