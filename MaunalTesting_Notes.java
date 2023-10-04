f3cc16955b9aef9eb621b2026ea0e10be32c1607
pradeepyalamasetti <105592177+pradeepyalamasetti@users.noreply.github.com>
2023-10-04 13:54:10 +0530

    Update MaunalTesting_Notes.java

--- MaunalTesting_Notes.java
+++ MaunalTesting_Notes.java
@@ -1,8 +1,18 @@
-1. What is Manual Testing?Maual testing
+1. What is Manual Testing?
+Ans. Manual Testing is a kind of software testing in which a software tester develops and executes the test cases without using any automated testing tools.
 2. What tool is used for Manual Testing?
+Ans. Manual testing tools such as Bugzilla, JIRA, JMeter, and many more.
 3. What is the Advantage of Manual Testing?
+Ans. 1.Manual testing can be done on all kinds of applications.
+     2.Application must be tested manually before it is automated.
+     3.It allows tester to perform adhoc testing
+     4.It is preferred in the projects where the requirements change frequently and for the products where the GUI changes constantly
 4. What are the disadvantages of Manual testing?
+Ans. 1.time-consuming
+     2.More number of human resource are required.
+     3.Repeating the same task again and again is almost impossiable.
 5. Will manual testing end in the future?
+Ans. 
 6. What is Regression Testing?
 7. How do you decide on test cases for the regression suite?
 8. How do you find the regression scenarios if a defect is fixed?
